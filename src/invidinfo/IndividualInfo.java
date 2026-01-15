1525e914b152c719467def1ff9110d2798875c98
Shannon Stephenson <49291863+neocleo1@users.noreply.github.com>
2026-01-14 21:59:35 -0500

    Initial commit: Add the individual's personal information program > Created simple Java program for printing fictional person's contact details

---
+++ src/invidinfo/IndividualInfo.java
@@ -0,0 +1,17 @@
+package invidinfo;
+
+public class IndividualInfo {
+	public static void main(String[] args) {
+		var firstName = "Sherlok";
+		var lastName = "Homes";
+		var streetAddress = "221 B Baker Street";
+		var city = "Loudon";
+		var zipCode = "VA";
+		
+		
+		System.out.println("First name: " + firstName );
+		System.out.println("Last name: " + lastName);
+		System.out.println("Street address: "+ streetAddress);
+		System.out.println("City: "+ city);
+		System.out.println("Zip code: "+ zipCode );
+}}
